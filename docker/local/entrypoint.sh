02bd96dc8bedbc3957e96c30fe1e5fc5f4dbcac9
Javier Méndez Veira <javier.mendez@netexlearning.com>
2021-09-09 17:04:45 +0200

    Refactor php config packages

--- docker/local/entrypoint.sh
+++ docker/local/entrypoint.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
-set -x
+#set -x
 
 # Configure .env.local.php (performance) after create .env from Configserver:
 php -d memory_limit=-1 ${COMPOSER_PATH} dump-env ${APP_ENV} --no-cache
 
+
+if [ "$XDEBUG_ACTIVATED" = true ] ; then
+    echo "[ENTRYPOINT] Enable xdebug php.ini package"
+    cp /docker-utilities/phpini/xdebug.ini /usr/local/etc/php/conf.d
+fi
+
+# if [ "$APP_EXECUTE_MIGRATIONS" = "true" ]; then
+
+#     #echo "[ENTRYPOINT] Update mongodb schema:"
+#     #php /var/www/app/bin/console doctrine:mongodb:schema:update --no-interaction
+#     # if [ $? -ne 0 ]; then
+#     #     echo "[ENTRYPOINT] ERROR: Mongodb Schema update can't be executed." > /dev/stderr
+#     #     exit 1
+#     # fi
+
+#     # echo "[ENTRYPOINT] Execute migrations"
+#  	# php /var/www/app/bin/console doctrine:migrations:migrate --no-interaction
+#  	# if [ $? -ne 0 ]; then
+#     #     echo "[ENTRYPOINT] ERROR: Migrations can't be executed." > /dev/stderr
+#     #     exit 1
+#     # fi
+# fi
+
+
+# Launch roadrunner with local config
+# TODO: move config to folder docker/{env}
 bin/rr serve -c .rr.dev.yaml
